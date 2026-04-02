3344b9939be7687ce86c7a66db66baff30f6acfa
Anastasiya Alekseeva <anastasiyaalexxx@gmail.com>
2026-04-02 11:10:38 +0300

    Добавление абстрактного класса

--- src/view/events-view.js
+++ src/view/events-view.js
@@ -1,22 +1,11 @@
-import { createElement } from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 function createBoardTemplate() {
   return '<ul class="trip-events__list"></ul>';
 }
 
-export default class TripEventsListView{
-  getTemplate() {
+export default class TripEventsListView extends AbstractView {
+  get template() {
     return createBoardTemplate();
-  }
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-    return this.element;
-  }
-
-  removeElement() {
-    this.element = null;
   }
 }
